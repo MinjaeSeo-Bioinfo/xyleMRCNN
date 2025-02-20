ef3af37fcdb5234ad5493cf0f67260002c53c859
minjaeseo <mj.seo@snu.ac.kr>
2025-02-20 12:17:42 +0900

    2520220 engine.py

--- pytorch_mask_rcnn/engine.py
+++ pytorch_mask_rcnn/engine.py
@@ -1,11 +1,10 @@
 import sys
 import time
-
 import torch
 
 from .utils import Meter, TextArea
 try:
-    from .datasets import CocoEvaluator, prepare_for_coco
+    from .datasets.xylem_eval import XylemEvaluator, prepare_for_xylem_coco
 except:
     pass
 
@@ -13,7 +12,7 @@ except:
 def train_one_epoch(model, optimizer, data_loader, device, epoch, args):
     for p in optimizer.param_groups:
         p["lr"] = args.lr_epoch
-
+    # args.iter < 0이면 전체 데이터셋을 학습
     iters = len(data_loader) if args.iters < 0 else args.iters
 
     t_m = Meter("total")
@@ -24,6 +23,7 @@ def train_one_epoch(model, optimizer, data_loader, device, epoch, args):
     for i, (image, target) in enumerate(data_loader):
         T = time.time()
         num_iters = epoch * len(data_loader) + i
+        # 안정적인 학습 보조 warmup_iter
         if num_iters <= args.warmup_iters:
             r = num_iters / args.warmup_iters
             for j, p in enumerate(optimizer.param_groups):
@@ -36,17 +36,18 @@ def train_one_epoch(model, optimizer, data_loader, device, epoch, args):
         losses = model(image, target)
         total_loss = sum(losses.values())
         m_m.update(time.time() - S)
-            
+        
         S = time.time()
         total_loss.backward()
         b_m.update(time.time() - S)
         
         optimizer.step()
         optimizer.zero_grad()
-
-        if num_iters % args.print_freq == 0:
+        
+        #@ add True for debugging
+        if num_iters % args.print_freq == 0 or True:
             print("{}\t".format(num_iters), "\t".join("{:.3f}".format(l.item()) for l in losses.values()))
-
+        
         t_m.update(time.time() - T)
         if i >= iters - 1:
             break
@@ -60,10 +61,11 @@ def evaluate(model, data_loader, device, args, generate=True):
     iter_eval = None
     if generate:
         iter_eval = generate_results(model, data_loader, device, args)
-
+    
     dataset = data_loader #
     iou_types = ["bbox", "segm"]
-    coco_evaluator = CocoEvaluator(dataset.coco, iou_types)
+    #@ CocoEvaluator to XylemEvaluator(from xylem_eval.py)
+    coco_evaluator = XylemEvaluator(dataset.coco, iou_types)
 
     results = torch.load(args.results, map_location="cpu")
 
@@ -105,7 +107,8 @@ def generate_results(model, data_loader, device, args):
         m_m.update(time.time() - S)
         
         prediction = {target["image_id"].item(): {k: v.cpu() for k, v in output.items()}}
-        coco_results.extend(prepare_for_coco(prediction))
+        #@ prepare_for_coco to prepare_for_xylem_coco(from xylem_eval.py)
+        coco_results.extend(prepare_for_xylem_coco(prediction))
 
         t_m.update(time.time() - T)
         if i >= iters - 1:
@@ -116,5 +119,3 @@ def generate_results(model, data_loader, device, args):
     torch.save(coco_results, args.results)
         
     return A / iters
-    
-
