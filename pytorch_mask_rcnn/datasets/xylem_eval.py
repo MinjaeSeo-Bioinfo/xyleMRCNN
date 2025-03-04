337eb02d2a79505f57dfd4959ade9203b5d1e1ce
MinjaeSeo <mj.seo@snu.ac.kr>
2025-03-04 16:15:03 +0900

    Update xylem_eval.py

--- pytorch_mask_rcnn/datasets/xylem_eval.py
+++ pytorch_mask_rcnn/datasets/xylem_eval.py
@@ -76,7 +76,7 @@ class XylemEvaluator:
         else:
             print("evaluation has no results")
     #@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-
+    
 def prepare_for_xylem_coco(predictions):
     coco_results = []
     for original_id, prediction in predictions.items():
@@ -92,9 +92,6 @@ def prepare_for_xylem_coco(predictions):
         boxes = boxes.tolist()
         scores = prediction["scores"].tolist()
         labels = prediction["labels"].tolist()
-        
-        #@
-        labels = [label - 1 for label in labels]
 
         masks = masks > 0.5
         rles = [
@@ -116,5 +113,5 @@ def prepare_for_xylem_coco(predictions):
                 for i, rle in enumerate(rles)
             ]
         )
-        
+
     return coco_results
