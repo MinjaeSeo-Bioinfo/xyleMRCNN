d2a47c58ba086555390d67dfd93c63eb17a8c2c0
MinjaeSeo <mj.seo@snu.ac.kr>
2025-03-07 16:00:59 +0900

    Update __init__.py

--- pytorch_mask_rcnn/model/__init__.py
+++ pytorch_mask_rcnn/model/__init__.py
@@ -1,4 +1,1 @@
-try:
-    from .mask_rcnn import maskrcnn_resnet50
-except ImportError:
-    from .mask_rcnn import maskrcnn_se_resnet50 as maskrcnn_resnet50
+from .mask_rcnn import maskrcnn_se_resnet50
